0700ba6a3dce40b2cc03a7583a64ec4de122ee47
Julen <jmontes@gaming.tech>
2025-11-03 16:48:51 +0100

    InitialCommit

---
+++ Source/ProjetFilRougePerso/ProjetFilRougePerso.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjetFilRougePerso.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjetFilRougePerso, "ProjetFilRougePerso" );
